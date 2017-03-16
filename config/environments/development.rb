105407eb02198454b1a9f12fbaf0411e7c0556ba
y5f <ysf.9203@gmail.com>
2017-03-16 03:39:01 +0100

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,13 +5,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :user_name => "ysf.9203@gmail.com",
-      :password => "*****",
-      :authentication => :plain,
-      :enable_starttls_auto => true
-  }
+
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'example.com',
+      user_name:            '<username>',
+      password:             '<password>',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+
   #config.action_mailer.delivery_method = :test
 
 
